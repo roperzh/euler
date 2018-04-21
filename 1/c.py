455393e978956d8c0cb04e3b82e85611d1a22c12
seq <pdntspa@protonmail.ch>
2018-04-20 22:28:20 -0300

    Added possibles solutions (c and d) to problem 1

---
+++ 1/c.py
@@ -0,0 +1,7 @@
+def addMultiple():
+	result = 0
+	for i in range(1000):
+		if i % 3 == 0 or i % 5 == 0:
+			result += i
+	return result
+print(addMultiple())
---
+++ 1/d.py
@@ -0,0 +1,4 @@
+def addMultiple():
+	return sum(i for i in range(1000) if i % 3 == 0 or i % 5 == 0)
+
+print(addMultiple())
