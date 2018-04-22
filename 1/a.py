872acc1a30d88a4588e6d5dd1a0df686776e5f05
seq <pdntspa@protonmail.ch>
2018-04-21 22:29:23 -0300

    Changed function to Python 3 syntax

--- 1/a.py
+++ 1/a.py
@@ -4,7 +4,7 @@ def is_multiple_of(number, base):
 def find_sum_below(limit):
   total = 0
 
-  for number in xrange(1,limit):
+  for number in range(1,limit):
     if is_multiple_of(number, 3) or is_multiple_of(number, 5):
       total += number
 
